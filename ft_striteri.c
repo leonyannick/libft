7ddbb65915c3dc88af04f7ed73bd8ee2fbc0347d
Leon Baumann <lbaumann@c4c5c2.42berlin.de>
2022-12-13 10:40:17 +0100

    change return in striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: lbaumann < lbaumann@student.42berlin.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 14:06:45 by lbaumann          #+#    #+#             */
-/*   Updated: 2022/12/13 10:28:23 by lbaumann         ###   ########.fr       */
+/*   Updated: 2022/12/13 10:38:26 by lbaumann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	int		i;
 
 	if (s == 0 || f == 0)
-		return (0);
+		return ;
 	i = 0;
 	while (s[i] != 0)
 	{
